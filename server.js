c587247f4bcda3481640c05613b59a9774eb6dcb
Evan Nye <evan.sonoranstudios@gmail.com>
2024-01-27 10:31:39 -0500

    prepared app for deployment again

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
-  // console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
 
 process.on('unhandledRejection', (err) => {
